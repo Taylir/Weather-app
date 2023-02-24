ed78cb514d00ac50c816d0df8fdea712dadd3446
Taylir <110134876+Taylir@users.noreply.github.com>
2023-02-24 14:04:52 -0500

    Switching to python for now and will come back to this.

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,36 @@ import axios from "axios";
 
 function App() {
   const [weather, setWeather] = useState(null);
-  const [userInput, setUserInput] = useState("Washington DC");
+  const [userInput, setUserInput] = useState("Washington Dc");
 
+  async function getWeather() {
+    const options = {
+      method: "GET",
+      url: "https://weatherapi-com.p.rapidapi.com/forecast.json",
+      params: { q: `${userInput}`, days: "3" },
+      headers: {
+        "X-RapidAPI-Key": "285110c9eamsh53fd7501a267f4fp1c913djsn6a7d37da6a81",
+        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        setWeather(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+        alert(
+          "Location not found, Please try again. Lat and Lon can only be 4 decimel places long."
+        );
+      });
+  }
   useEffect(() => {
-    async function getWeather() {
-      const options = {
-        method: "GET",
-        url: "https://weatherapi-com.p.rapidapi.com/forecast.json",
-        params: { q: `${userInput}`, days: "3" },
-        headers: {
-          "X-RapidAPI-Key":
-            "285110c9eamsh53fd7501a267f4fp1c913djsn6a7d37da6a81",
-          "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
-        },
-      };
-
-      axios
-        .request(options)
-        .then(function (response) {
-          console.log(response.data);
-          setWeather(response.data);
-        })
-        .catch(function (error) {
-          console.error(error);
-        });
-    }
-
     getWeather();
-  }, [userInput]);
+    setUserInput("");
+  }, []);
 
   if (!weather) {
     return <div>Loading...</div>;
@@ -42,11 +44,12 @@ function App() {
       <div>
         <img src={`https:${weather.current.condition.icon}`} alt="" />
       </div>
-      <div>{`The temprature in ${weather.location.name}, ${weather.location.region} is ${weather.current.temp_f} f°`}</div>
+      <div>{`The tempature in ${weather.location.name}, ${weather.location.region} is ${weather.current.temp_f} f°`}</div>
       <form
         onSubmit={(e) => {
           e.preventDefault();
           console.log(userInput);
+          getWeather();
         }}
       >
         <input
@@ -54,11 +57,17 @@ function App() {
           placeholder="City, Zip, or (Lat, Lon)"
           value={userInput}
           onChange={(e) => {
+            console.log(userInput);
             setUserInput(e.target.value);
           }}
         />
         <button type="submit">Check Weather!</button>
       </form>
+      <div className="weather__hours">
+        {weather.forecast.forecastday["0"].hour.map((snow) => (
+          <div>{snow.chance_of_snow}</div>
+        ))}
+      </div>
     </div>
   );
 }
