74b17e35c06c113f40468ee6abee0e2517a10c4d
Taylir <110134876+Taylir@users.noreply.github.com>
2023-02-24 12:51:16 -0500

    Got the basic serch function finished will be adding more options

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,66 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  async function getWeather() {
-    const { data } = await axios.get(
-      `https://api.weather.gov/gridpoints/HUN/41.203323,-77.194527/forecast`
-    );
-    console.log(data);
+  const [weather, setWeather] = useState(null);
+  const [userInput, setUserInput] = useState("Washington DC");
+
+  useEffect(() => {
+    async function getWeather() {
+      const options = {
+        method: "GET",
+        url: "https://weatherapi-com.p.rapidapi.com/forecast.json",
+        params: { q: `${userInput}`, days: "3" },
+        headers: {
+          "X-RapidAPI-Key":
+            "285110c9eamsh53fd7501a267f4fp1c913djsn6a7d37da6a81",
+          "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+        },
+      };
+
+      axios
+        .request(options)
+        .then(function (response) {
+          console.log(response.data);
+          setWeather(response.data);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    }
+
+    getWeather();
+  }, [userInput]);
+
+  if (!weather) {
+    return <div>Loading...</div>;
   }
 
-  getWeather();
-
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>
+        <img src={`https:${weather.current.condition.icon}`} alt="" />
+      </div>
+      <div>{`The temprature in ${weather.location.name}, ${weather.location.region} is ${weather.current.temp_f} f°`}</div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(userInput);
+        }}
+      >
+        <input
+          type="text"
+          placeholder="City, Zip, or (Lat, Lon)"
+          value={userInput}
+          onChange={(e) => {
+            setUserInput(e.target.value);
+          }}
+        />
+        <button type="submit">Check Weather!</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
